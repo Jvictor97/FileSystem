eca9870c66fe643914306c544ca59ef9197e7ea8
Joao Souza <jvictor.9@hotmail.com>
2018-08-12 20:07:06 -0300

    Desenvolvimento inicial

--- main.cpp
+++ main.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
+#include <string>
+#include <stdio.h>
+#include <map>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+vector<int> getclause();
+void createhd(string, int, int);
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    getclause();
+    system("pause");
     return 0;
+}
+
+void createhd(string name, int bsize, int blocks){
+    FILE * file;
+    file = fopen("teste.txt", "a");
+    if (file != NULL)
+        cout<<"Arquivo aberto!\n";
+}
+
+vector<int> getclause() {
+  char c;
+  vector<int> cl;
+  std::string line;
+  std::getline(cin, line);
+  std::istringstream iss(line);
+  while ( iss >> c) {    
+    cout<<c<<" ";
+  }
+  return cl;
 }
